e0a9d3e170ac5b20d452f19d6297a2bd556cdc91
Jason Choi <jasonchoi.mtt@gmail.com>
2015-05-10 21:21:35 +0800

    Initial commit.

---
+++ project/settings.py
@@ -0,0 +1,81 @@
+"""
+Django settings for project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+import os
+import socket
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+DATA_DIR = os.environ.get('OPENSHIFT_DATA_DIR', BASE_DIR)
+
+
+SECRET_KEY = os.environ.get('OPENSHIFT_SECRET_TOKEN',
+                            '-g11@ryqhjtd1_(rgrto!*mje8(t!40o$ubx#68=*m=j67@2+%')
+
+DEBUG = 'OPENSHIFT_REPO_DIR' not in os.environ
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = []
+if 'OPENSHIFT_REPO_DIR' in os.environ:
+    ALLOWED_HOSTS += [os.environ['OPENSHIFT_APP_DNS'], socket.gethostname()]
+
+
+INSTALLED_APPS = (
+    'grappelli',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.core.context_processors.request',  # needed by grappelli
+    'django.contrib.messages.context_processors.messages',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'project.urls'
+
+WSGI_APPLICATION = 'project.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(DATA_DIR, 'db.sqlite3'),
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'Asia/Hong_Kong'
+
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.environ.get('OPENSHIFT_REPO_DIR', BASE_DIR)
